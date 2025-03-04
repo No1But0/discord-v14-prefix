93e3156436be65ef9f698a32067ecf76ee66b91a
No1But0 <zddzzddzdz18@gmail.com>
2025-03-04 13:21:00 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    token: '',
-    prefix: '+',
-    embedColor: '#ff0000',
-    name: 'Gestion',
-    logo: 'https://cdn.discordapp.com/attachments/1333328253104160853/1333328323933372416/telechargement_7.jpg?ex=67987e44&is=67972cc4&hm=8e6bc09d3b64ce5a0582c13acae1cbbbb75c64f60a79f631d3aea451332caef8&',
-    buyer: '1151211333489016882',
+    token: '', // Token
+    prefix: '+', // Prefix 
+    embedColor: '#ff0000', // Color
+    name: '', // Footer Text
+    logo: '', // Footer Logo
+    buyer: '', // Your ID
     perm: "❌ Vous n'avez pas la permission d'executer cette commande.",
     error: "⚠️ Erreur lors de l'execution de la commande.",
     bot_id: ''
